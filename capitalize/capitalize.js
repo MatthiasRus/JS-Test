6197edae30cae7efcef9fed681e7f91bffc44a5b
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-30 15:33:52 +0400

    The test cases added tho simple

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,4 +1,7 @@
 function capitalize(str){
+  if (!/[A-Za-z]/.test(str)){
+    return "Input has to be Alphabet Only"
+  }
   return str.charAt(0).toUpperCase() + str.slice(1);
 } ;
 module.exports = capitalize;
--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,3 +1,11 @@
 const capitalize = require("./capitalize.js");
 
 it('working',() => {expect(capitalize('asmmamaw')).toBe("Asmmamaw")});
+test('Not working',()=> {
+  expect(capitalize('')).toBe("Input has to be Alphabet Only")
+});
+test('Not working',()=> {
+  expect(capitalize('12')).toBe("Input has to be Alphabet Only")
+})
+it('working',() => {expect(capitalize('asmma233')).toBe("Asmma233")});
+
